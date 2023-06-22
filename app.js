14ebfd25d15af777113eecca0fbb6c5de70295b9
laura <laupri@gmail.com>
2023-06-23 00:18:53 +0200

    agregada ruta a archivos html estaticos

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require ('express');
 const app = express();
 const PORT = 3002;
+
 //middelware o capa que controla el acceso a los archivos publicos
 app.use(express.static('public'));
 
 app.get("/",(req, res)=>{
     res.send("hola, el orden importa ");
+});
+
+app.get("/contacto",(req, res)=>{
+    res.sendFile(__dirname+'/contacto.html');
 });
 
 
