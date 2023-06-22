9551b61ea7e559f4912b40b403d6ae91ce1336ce
laura <laupri@gmail.com>
2023-06-22 13:43:55 +0200

    agregando rutas con express js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require ('express');
 const app = express();
 const PORT = 3002;
-
-// app.get("/",(req, res)=>{
-//     res.send("hola sadas ");
-// });
-
+//middelware o capa que controla el acceso a los archivos publicos
 app.use(express.static('public'));
+
+app.get("/",(req, res)=>{
+    res.send("hola, el orden importa ");
+});
+
+
 
 app.listen (PORT, ()=>console.log(`Servidor en http://localhost:${PORT}`));
